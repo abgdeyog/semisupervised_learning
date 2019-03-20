9ab4f79ce7bb0e03d3fdf5e844eead517b9b13f9
abgdeyog <abgdeyog@gmail.com>
2019-03-20 12:50:36 +0300

    sigma search with gradient descent

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from networkx.algorithms import node_classification
 import numpy as np
 import pandas as pd
 import math
+import random
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
@@ -36,20 +37,111 @@ def create_toroidal(k):
         G.add_edge(mapping2[(i,j)],mapping2[i+k-1,j])
     return G
 
+def find_sigma(Graph, label_name):
+    sigma = 0.5
+    sigma_new = 0.6
+    precision = 0
+    precision_new = 0
+    step_multiplier = 0.01
+    while abs(sigma - sigma_new) > 10e-6:
+        weights = {}
+        for key in Graph.nodes:
+            shortest_path = nx.algorithms.shortest_path(Graph, key)
+            for dest in shortest_path:
+                weight = math.exp(-len(shortest_path[dest]))
+                weights[(key, dest)] = weight
+        for key_pair in weights:
+            weights[key_pair] *= sigma
+        fu = {}
+        fl = {}
+        uniq_labeles = {}
+        uniq_labeles_n = 0
+        uniq_labeles_inverse = []
+        for key in Graph.nodes:
+            try:
+                label = Graph.node[key][label_name]
+                if label not in uniq_labeles:
+                    uniq_labeles[label] = uniq_labeles_n
+                    uniq_labeles_inverse.append(label)
+                    uniq_labeles_n += 1
+                fl[key] = uniq_labeles[label]
+            except KeyError:
+                fu[key] = ""
+        fl_train = {}
+        fl_test = {}
+        fu_train = fu.copy()
+        for i in fl:
+            if random.random() > 0.3:
+                fl_train[i] = fl[i]
+            else:
+                fl_test[i] = fl[i]
+                fu_train[i] = fl[i]
+
+        fu_train_list = [key for key in fu] + [key for key in fl_test]
+        fl_train_list = [key for key in fl_train]
+        fl_test_list = [key for key in fl_test]
+        Duu = np.zeros((len(fu_train), len(fu_train)))
+        Wuu = np.zeros((len(fu_train), len(fu_train)))
+        Wul = np.zeros((len(fu_train), len(fl_train)))
+        row = 0
+        for u_index in fu_train_list:
+            column = 0
+            for l_index in fl_train_list:
+                try:
+                    Wul[row][column] = weights[(u_index, l_index)]
+                except KeyError:
+                    pass
+                column += 1
+            column = 0
+            for u_index2 in fu_train_list:
+                try:
+                    Wuu[row][column] = weights[(u_index, u_index2)]
+                except KeyError:
+                    pass
+                column += 1
+            row += 1
+        for row in range(len(fu_train)):
+            sum = 0
+            for column in range(len(fu_train)):
+                sum += Wuu[row][column]
+            for column in range(len(fl_train)):
+                sum += Wul[row][column]
+            Duu[row][row] = sum
+        fl_values = np.array([fl[key] for key in fl_train])
+        fu_values = np.dot(np.dot(np.linalg.inv(Duu - Wuu), Wul), fl_values)
+
+        for row in range(len(fu_values)):
+            fu_train[fu_train_list[row]] = fu_values[row]
+        labeled_list = []
+        for i in range(len(fu_train) + len(fl_train)):
+            try:
+                labeled_list.append(fl_train[i])
+            except KeyError:
+                labeled_list.append(fu_train[i])
+        correct = 0
+        for key in fl_test:
+            if int(round(fu_train[key])) == fl_test[key]:
+                correct += 1
+        precision = precision_new
+        precision_new = correct / len(fl_test)
+        step = sigma_new - sigma
+        sigma = sigma_new
+        sigma_new = sigma + (precision_new - precision)/step * step_multiplier
+        print(sigma)
+    return sigma_new
+
 def your_harmonic_function(Graph, label_name):
     # YOUR CODE HERE
-    x = nx.spring_layout(G, iterations=200)
+    sigma = find_sigma(Graph, label_name)
+    print(sigma)
     weights = {}
-    max_dist = 0
     for key in Graph.nodes:
         shortest_path = nx.algorithms.shortest_path(Graph, key)
         for dest in shortest_path:
             weight = math.exp(-len(shortest_path[dest]))
             weights[(key, dest)] = weight
-            if weight > max_dist:
-                max_dist = weight
     for key_pair in weights:
-        weights[key_pair] /= max_dist
+        weights[key_pair] *= sigma
     fu = {}
     fl = {}
     uniq_labeles = {}
